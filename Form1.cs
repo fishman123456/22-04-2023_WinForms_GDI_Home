2db879f6856cae70522e4a3efcda7fe365299f7a
fishman123456 <den9152@yandex.ru>
2023-04-24 23:14:53 +0700

    bmp вывел

--- Form1.cs
+++ Form1.cs
@@ -8,6 +8,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml.Linq;
 
 namespace _22_04_2023_WinForms_GDI_Home
 {
@@ -41,39 +42,39 @@ namespace _22_04_2023_WinForms_GDI_Home
         //    g.DrawString("Hello World!", f, Brushes.Blue, 10, 10);
         //    //g.Dispose();
         //}
-        private void btnSave_Click(object sender, EventArgs e)
-        {
-            try
-            {
-                Bitmap myBitmap = new Bitmap("Background.bmp");
-                // получаем объект Graphics
-                Graphics gFromImage = Graphics.FromImage(myBitmap);
-                Font f = new Font("Verdana", 8, FontStyle.Italic);
-                string helloStr = "Hello World!";
-                // меряем "Hello World!" с помошью метода MeasureString
-                SizeF sz = gFromImage.MeasureString(helloStr, f);
-                gFromImage.DrawString("Hello World!", f, Brushes.Blue, 10, 10);
-                gFromImage.DrawRectangle(new Pen(Color.Red, 2),
-                 10.0F, 10.0F, sz.Width, sz.Height);
-                // сохроняем изображение на диск
-                myBitmap.Save(@"NewFon.bmp");
-                Rectangle regionRec = new Rectangle(new Point(0, 0), myBitmap.Size);
-                myBitmap.Dispose();
-                gFromImage.Dispose();
-                // этот метод выполняет перерисовку клиентской области
-                this.Invalidate(regionRec);
-            }
-            catch { }
-        }
+        //private void btnSave_Click(object sender, EventArgs e)
+        //{
+        //    try
+        //    {
+        //        Bitmap myBitmap = new Bitmap("Background.bmp");
+        //        // получаем объект Graphics
+        //        Graphics gFromImage = Graphics.FromImage(myBitmap);
+        //        Font f = new Font("Verdana", 8, FontStyle.Italic);
+        //        string helloStr = "Hello World!";
+        //        // меряем "Hello World!" с помошью метода MeasureString
+        //        SizeF sz = gFromImage.MeasureString(helloStr, f);
+        //        gFromImage.DrawString("Hello World!", f, Brushes.Blue, 10, 10);
+        //        gFromImage.DrawRectangle(new Pen(Color.Red, 2),
+        //         10.0F, 10.0F, sz.Width, sz.Height);
+        //        // сохроняем изображение на диск
+        //        myBitmap.Save(@"NewFon.bmp");
+        //        Rectangle regionRec = new Rectangle(new Point(0, 0), myBitmap.Size);
+        //        myBitmap.Dispose();
+        //        gFromImage.Dispose();
+        //        // этот метод выполняет перерисовку клиентской области
+        //        this.Invalidate(regionRec);
+        //    }
+        //    catch { }
+        //}
 
         private void Form1_Paint_1(object sender, PaintEventArgs e)
         {
             try
             {
 
-                Bitmap myBitmap = new Bitmap("NewBackground.bmp");
+                var myBitmap = new Bitmap(@"C:\Users\Fishman_1\Documents\GitHub\22-04-2023_WinForms_GDI_Home\Background.bmp");
                 Graphics g = e.Graphics;
-                g.DrawImage(myBitmap, 0, 0, 300, 200);
+                g.DrawImage(myBitmap, 10, 10, 300, 200);
                 myBitmap.Dispose();
                 g.Dispose();
             }
